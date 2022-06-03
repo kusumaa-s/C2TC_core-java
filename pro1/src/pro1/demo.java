2c34ca357f3af79ee1fcdab88a95fa5895ec1544
Kusuma <Kusuma@DESKTOP-D6PIEG7>
2022-06-03 14:02:43 +0530

    first commit

---
+++ pro1/src/pro1/demo.java
@@ -0,0 +1,17 @@
+package pro1;
+
+public class demo {
+	int x;
+}
+
+class test demo  
+
+	public static void main(String args[]) 
+	{
+	demo  myObj =new demo ();
+	myObj.x=40;
+	
+System. out.println (myObj.x);
+
+}
+}
